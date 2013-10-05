104467635d85c035f64c229448c8825672974cb3
unknown <jerik@STUD6-H56.d.ethz.ch>
2013-10-05 14:30:15 +0200

    Added client transport and client library

--- code/client.tests/src/SocketTransportTests.java
+++ code/client.tests/src/SocketTransportTests.java
@@ -1,16 +1,13 @@
-import static org.junit.Assert.*;
-
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import asl.SocketTransport;
+import asl.network.SocketTransport;
 
 @RunWith(JUnit4.class)
 public class SocketTransportTests {
@@ -18,14 +15,14 @@ public class SocketTransportTests {
     @Test
     public void ShouldBeAbleToSendAndRecieveData() throws UnknownHostException, IOException {
     	// Arrange
-    	Socket s = new Socket("www.google.com", 80);
-    	SocketTransport transport = new SocketTransport(s);
-    	String stringToSend = "GET / HTTP/1.0\r\nHost: www.google.com\r\n\r\n";
+    	Socket s = new Socket("maddox.xmission.com", 80);
+    	SocketTransport transport = new SocketTransport(s); 
+    	String stringToSend = "GET / HTTP/1.0\r\nHost: maddox.xmission.com\r\n\r\n";
     	
     	// Act
     	String result = transport.SendAndGetResponse(stringToSend);
     	
     	// Assert
-    	Assert.assertTrue(result.contains("HTTP"));
+    	Assert.assertTrue(result.contains("200 OK"));
     }
 }
