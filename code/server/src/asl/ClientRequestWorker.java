5045c43479e2e2d91d630dbf07e46d78de9472a0
Michael Bang <michael@katnegermis.dk>
2013-10-05 13:30:55 +0200

    Add partial implementation of server side nonblocking sockets and threadpooling

---
+++ code/server/src/asl/ClientRequestWorker.java
@@ -0,0 +1,16 @@
+package asl;
+
+import java.nio.channels.SelectionKey;
+
+public class ClientRequestWorker implements Runnable{
+
+	public ClientRequestWorker(String message, SelectionKey conn) {
+
+	}
+	
+	@Override
+	public void run() {
+		
+	}
+
+}
--- code/server/src/asl/Main.java
+++ code/server/src/asl/Main.java
@@ -1,12 +1,19 @@
 package asl;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		ExecutorService threadpool = Executors.newFixedThreadPool(ASLServerSettings.NUM_CLIENTREQUESTWORKER_THREADS);
+		// Do stuff
+		
+		// Close threads gracefully. 
+		threadpool.shutdown();
 	}
 
 }
