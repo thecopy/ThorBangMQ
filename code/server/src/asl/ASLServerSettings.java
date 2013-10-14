6c7166c08265c9f6a8b8ada632e269c726c9fb17
Michael Bang <michael@katnegermis.dk>
2013-10-14 22:13:12 +0200

    Fix typo in variable name. Add parameters to constructor.

--- code/server/src/asl/ASLServerSettings.java
+++ code/server/src/asl/ASLServerSettings.java
@@ -4,7 +4,18 @@ import java.nio.charset.Charset;
 
 public class ASLServerSettings {
 	public static final int SOCKET_PORT = 8123;
-	public static final int NUM_CLIENTREQUESTWORKER_THREADS = 20;
 	public static final Charset CHARSET = Charset.forName("UTF-8");
-	public static final int MESSAGE_MAX_LENGTH = 4096;
+		
+	public static int MESSAGE_MAX_LENGTH = 4096;
+	public static int NUM_CLIENTREQUESTWORKER_THREADS = 20;
+
+	public static String DB_SERVER_NAME = "localhost";
+	public static String DB_DATABASE_NAME = "asl";
+	public static String DB_DATA_SOURCE_NAME = "asl";
+	public static int DB_PORT_NUMBER = 5432;
+	public static String DB_USERNAME = "asl";
+	public static String DB_PASSWORD = "asl2013";
+	public static int DB_MAX_CONNECTIONS = 10;
+	public static boolean DB_DEFAULT_AUTO_COMMIT = false;
+	
 }
