83c1e0481a9f4d78bb7ebd6960297c057fad1a4f
Erik Jonsson Thorén <thecopy@gmail.com>
2013-10-15 13:34:15 +0200

    Implemented server side protocol

--- code/server/src/asl/ASLClientRequestWorker.java
+++ code/server/src/asl/ASLClientRequestWorker.java
@@ -34,6 +34,7 @@ public class ASLClientRequestWorker implements Runnable{
 		}
 	}
 
+	// TODO handle exceptions
 	private void interperter(String msg) throws IOException{
 		String[] methodWithArgs = msg.split(",", 2);
 
@@ -41,17 +42,27 @@ public class ASLClientRequestWorker implements Runnable{
 		case "HELLO":
 			transport.Send("OK");
 			break;
-		case "SEND":
+		case "MSG":
 			storeMessage(methodWithArgs[1]);
 			break;
 		case "PEEKQ":
-			Message m = peekMessage(methodWithArgs[1]);
-			if(m == null)
-			{
-				transport.Send("MSG0");
-			}else{
-				transport.Send(String.format(SendMessageStringFormat, m.senderId, m.content, m.id, m.content));
-			}
+			sendMessage(peekQueue(methodWithArgs[1]));
+			break;
+		case "PEEKS":
+			sendMessage(peekQueueWithSender(methodWithArgs[1]));
+			break;
+		case "POPQ":
+			sendMessage(popQueue(methodWithArgs[1]));
+			break;
+		case "POPS":
+			sendMessage(popQueueWithSender(methodWithArgs[1]));
+			break;
+		case "CREATEQUEUE":
+			transport.Send(String.valueOf(createQueue(methodWithArgs[1])));
+			break;
+		case "REMOVEQUEUE":
+			removeQueue(Long.parseLong(methodWithArgs[1]));
+			transport.Send("OK");
 			break;
 		default:
 			logger.log(Level.WARNING, "Failed to interpert client message: " + msg);
@@ -59,14 +70,15 @@ public class ASLClientRequestWorker implements Runnable{
 		}
 	}
 
+	// MSG,ReceiverId,SenderId,QueueId,Priority,Context,Content
 	private void storeMessage(String argsConcat){
 		String[] args = argsConcat.split(",", 6);
 
 		long receiver = Long.parseLong(args[0]);
 		long sender = Long.parseLong(args[1]);
 		long queue = Long.parseLong(args[2]);
-		long context = Long.parseLong(args[3]);
-		int priority = Integer.parseInt(args[4]);
+		int priority = Integer.parseInt(args[3]);
+		long context = Long.parseLong(args[4]);
 		String content = args[5];
 
 		Message message = new Message(receiver, sender, 0, queue, 0, priority, context, content);
@@ -74,16 +86,79 @@ public class ASLClientRequestWorker implements Runnable{
 		persistence.storeMessage(message);
 	}
 
-	private Message peekMessage(String argsConcat){
+	// PEEKQ,ReceiverId,QueueId,OrderByTimestampInsteadPriority
+	private Message peekQueue(String argsConcat){
 		String[] args = argsConcat.split(",", 3);
 
 		long receiver = Long.parseLong(args[0]);
 		long queue = Long.parseLong(args[1]);
 		Boolean getByTimestampInsteadOfPriority = Integer.parseInt(args[2]) == 1;
 
+		Message m = null;
 		if(getByTimestampInsteadOfPriority)
-			return persistence.getMessageByTimestamp(queue, receiver);
+			m = persistence.getMessageByTimestamp(queue, receiver);
 		else
-			return persistence.getMessageByPriority(queue, receiver);
+			m = persistence.getMessageByPriority(queue, receiver);
+		
+		return m;
+	}
+	
+	
+	// PEEKS,ReceiverId,QueueId,SenderId,OrderByTimestampInsteadPriority
+	private Message peekQueueWithSender(String argsConcat){
+		String[] args = argsConcat.split(",", 4);
+
+		long receiver = Long.parseLong(args[0]);
+		long queue = Long.parseLong(args[1]);
+		long sender = Long.parseLong(args[2]);
+		Boolean getByTimestampInsteadOfPriority = Integer.parseInt(args[3]) == 1;
+
+		// TODO: Differentiate on getByTimestampInsteadOfPriority
+		Message m = null;
+		m = persistence.getMessageBySender(queue, receiver, sender);
+		
+		return m;
+	}
+	
+	// POPQ,ReceiverId,QueueId,OrderByTimestampInsteadPriority
+	private Message popQueue(String argsConcat){
+		Message m = peekQueue(argsConcat);
+		
+		if(m != null)
+			persistence.deleteMessage(m.id);
+		
+		return m;
+	}
+
+	// POPQ,ReceiverId,QueueId,OrderByTimestampInsteadPriority
+	private Message popQueueWithSender(String argsConcat){
+		Message m = peekQueueWithSender(argsConcat);
+		
+		if(m != null)
+			persistence.deleteMessage(m.id);
+		
+		return m;
+	}
+	
+	
+	// CREATEQUEUE,NameOfQueue
+	private long createQueue(String name){
+		return persistence.createQueue(name);
+	}
+	
+	// REMOVEQUEUE,QueueId
+	private void removeQueue(long id){
+		persistence.removeQueue(id);
+	}
+	
+	private void sendMessage(Message m){
+		if(m == null)
+			transport.Send("MSG0");
+		else
+			transport.Send(formatMessage(m));
+	}
+	
+	private String formatMessage(Message m){
+		return String.format(SendMessageStringFormat, m.senderId, m.content, m.id, m.content);
 	}
 }
