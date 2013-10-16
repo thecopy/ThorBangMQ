cded844543008ec04b75517d7d4c93f8d01ed50c
Michael Bang <michael@katnegermis.dk>
2013-10-16 14:15:04 +0200

    Removed unused imports.

--- code/server/src/asl/infrastructure/Bootstrapping.java
+++ code/server/src/asl/infrastructure/Bootstrapping.java
@@ -9,8 +9,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import asl.ASLServerSettings;
-import asl.Persistence.IPersistence;
-import asl.Persistence.InMemoryPersistence;
 
 public class Bootstrapping {
 	private static String useMemoryPersistenceSetting = "useinmemorypersistence";
