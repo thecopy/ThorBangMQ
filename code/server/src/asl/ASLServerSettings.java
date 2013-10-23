46a1aa79540bbc499327679349ac577aab44fc09
Michael Bang <michael@katnegermis.dk>
2013-10-23 15:21:21 +0200

    Change settings to match those on the server.

--- code/server/src/asl/ASLServerSettings.java
+++ code/server/src/asl/ASLServerSettings.java
@@ -11,11 +11,11 @@ public class ASLServerSettings {
 	public int NUM_CLIENTREQUESTWORKER_THREADS = 20;
 
 	public String DB_SERVER_NAME = "localhost";
-	public String DB_DATABASE_NAME = "asl-test";
+	public String DB_DATABASE_NAME = "asl";
 	public String DB_DATA_SOURCE_NAME = "asl";
 	public int DB_PORT_NUMBER = 5432;
 	public String DB_USERNAME = "asl";
-	public String DB_PASSWORD = "";
+	public String DB_PASSWORD = "asl2013";
 	public int DB_MAX_CONNECTIONS = 10;
 	public boolean DB_DEFAULT_AUTO_COMMIT = false;
 
