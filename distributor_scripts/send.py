75c857f04b60dd0461d331920d90141726215f84
Erik Jonsson Thorén <thecopy@gmail.com>
2013-10-29 16:00:27 +0100

    Added menu to send.py

--- distributor_scripts/send.py
+++ distributor_scripts/send.py
@@ -1,19 +1,43 @@
 import logging
 
 from utils import buildjavafiles, distributefiles, startservers, startclients
-from droplets import droplets, getclients, getservers, getdatabase
+from droplets import getdroplets, getclients, getservers, getdatabase, createclient, createserver, destroyalldroplets
 
 logging.basicConfig(level=logging.INFO)
 
-CLIENTS = getclients()
-SERVERS = getservers()
-DATABASE = getdatabase()
+while(True):
+    print "Choose an option"
+    print "1) create client"
+    print "2) create server"
+    print "3) build jars"
+    print "4) upload jars"
+    print "5) start test <testname>"
+    print "6) list available tests"
+    print "7) list running droplets"
 
-for droplet in droplets:
-    logging.info('{name}: ({globalip}, {localip})'.format(name=droplet.name, globalip=droplet.ip_address, localip=droplet.private_ip_address))
-buildjavafiles()
-distributefiles(clients=CLIENTS, servers=SERVERS)
-startservers(servers=SERVERS, database=DATABASE, logginglevel='none',
-             db_connections=15, worker_threads=20)
-startclients(clients=CLIENTS, server_ip=SERVERS[0][1], test_name="writetest",
-             client_threads=20, msgs_per_client=30000)
+    choice = raw_input()
+
+    if choice == "1":
+        createclient()
+    elif choice == "2":
+        createserver()
+    elif choice == "3":
+        buildjavafiles()
+    elif choice == "4":
+        distributefiles(clients=getclients(), servers=getservers())
+    elif choice == "5":
+        print "not implemented :("
+    elif choice == "6":
+        print "not implemented :("
+    elif choice == "7":
+        for droplet in getdroplets():
+            logging.info('{name}: ({globalip}, {localip})'.format(name=droplet.name, globalip=droplet.ip_address, localip=droplet.private_ip_address))
+
+
+    print ""
+    print "----------------"
+    print ""
+    # startservers(servers=SERVERS, database=DATABASE, logginglevel='none',
+    #              db_connections=15, worker_threads=20)
+    # startclients(clients=CLIENTS, server_ip=SERVERS[0][1], test_name="writetest",
+    #              client_threads=20, msgs_per_client=30000)
