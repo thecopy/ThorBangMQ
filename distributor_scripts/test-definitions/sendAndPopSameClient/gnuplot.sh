a867bad1ff94b56222c6fd8983f3e1e5d77efa79
Michael Bang <michael@katnegermis.dk>
2013-11-09 15:02:12 +0100

    Change text on gnuplot for sendAndPopSameClient.

--- distributor_scripts/test-definitions/sendAndPopSameClient/gnuplot.sh
+++ distributor_scripts/test-definitions/sendAndPopSameClient/gnuplot.sh
@@ -7,6 +7,6 @@ set xrange [0:]
 set yrange [0:]
 set datafile separator ",";
 set xlabel 'Time since test started (milliseconds)';
-set ylabel 'Number of messages';
+set ylabel 'Messages per second';
 plot testfile using 1:2 with lines title "SendMessage", testfile using 1:3 with lines title "PopMessage";
 EOF
